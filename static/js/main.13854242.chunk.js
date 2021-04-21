1b316088f05223074833a44c922a268b208c34b9
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-04-21 19:58:21 +0300

    Updates

---
+++ static/js/main.13854242.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{4:function(t,e){},5:function(t,e,o){"use strict";o.r(e);var n=o(0),c=o.n(n),r=o(3),i=o.n(r),s=o(4),a=o.n(s),h=o(2);i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(a.a,{})}),document.getElementById("root"))}},[[5,1,2]]]);
+//# sourceMappingURL=main.13854242.chunk.js.map
