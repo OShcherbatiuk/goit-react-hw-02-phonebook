7461fbc0665573fb069b48cc1ee429c3e51d902f
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-04-27 01:53:44 +0300

    upd

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+// import { PropTypes } from 'prop-types';
+import shortid from 'shortid';
+
+const inputId = shortid.generate();
+
+const Filter = ({ value, onChange }) => (
+  <label htmlFor={inputId}>
+    Find contacts by name
+    <input id={inputId} type="text" value={value} onChange={onChange} />
+  </label>
+);
+
+export default Filter;
